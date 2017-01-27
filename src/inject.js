0a0e164f9baedfaa83ab941deeff38231fa2fc95
noraworld <noraworld@mail.com>
2017-01-27 21:33:38 +0900

    :fire: Delete submit event listener
    - This logic will be used to prevent submitting from website side JavaScript

--- src/inject.js
+++ src/inject.js
@@ -2,22 +2,10 @@
 
   'use strict';
 
-  var justBeforeKey = "";
-
   window.addEventListener('keydown', function(event) {
     justBeforeKey = event.key;
     if (event.key === 'Enter' && document.activeElement.nodeName === 'INPUT' && document.activeElement.getAttribute('type') === 'text') {
       let confirm = window.prompt('Are you sure you want to submit? Type "sure" if you want to.');
-      if (confirm !== 'sure') {
-        event.preventDefault();
-        return false;
-      }
-    }
-  }, true);
-
-  window.addEventListener('submit', function(event) {
-    if (justBeforeKey === 'Enter') {
-      let confirm = window.prompt('You pressed the enter key before submitting a form. Are you sure you want to submit? Type "sure" if you want to');
       if (confirm !== 'sure') {
         event.preventDefault();
         return false;
