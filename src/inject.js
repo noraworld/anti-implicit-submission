10b55549508ffadcb2e41e2d83e49b199c78c8e2
noraworld <noraworld@mail.com>
2017-01-27 21:56:14 +0900

    :fire: No confirmation before submitting

--- src/inject.js
+++ src/inject.js
@@ -4,11 +4,8 @@
 
   window.addEventListener('keydown', function(event) {
     if (event.key === 'Enter' && document.activeElement.nodeName === 'INPUT' && document.activeElement.getAttribute('type') === 'text') {
-      let confirm = window.prompt('Are you sure you want to submit? Type "sure" if you want to.');
-      if (confirm !== 'sure') {
-        event.preventDefault();
-        return false;
-      }
+      event.preventDefault();
+      return false;
     }
   }, true);
 
