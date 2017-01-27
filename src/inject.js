784fe5aa9d94d3e7485d65ba75ca01f8ddeb28f5
noraworld <noraworld@mail.com>
2017-01-27 21:38:27 +0900

    :bug: Delete undeclared variable

--- src/inject.js
+++ src/inject.js
@@ -3,7 +3,6 @@
   'use strict';
 
   window.addEventListener('keydown', function(event) {
-    justBeforeKey = event.key;
     if (event.key === 'Enter' && document.activeElement.nodeName === 'INPUT' && document.activeElement.getAttribute('type') === 'text') {
       let confirm = window.prompt('Are you sure you want to submit? Type "sure" if you want to.');
       if (confirm !== 'sure') {
