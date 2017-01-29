d9abf29437483dc9caa92a1d73d8d1798478eb95
noraworld <noraworld@mail.com>
2017-01-29 19:16:18 +0900

    :sparkles: Make a significant specification changes
    - Once pressing the enter key, it is ignored. But twice, confirm submission
    
    - If a user types... 'sure': submission is done. 'disable': submit the form without confirmation from the next time at the same URL

--- src/inject.js
+++ src/inject.js
@@ -2,11 +2,63 @@
 
   'use strict';
 
+  let exceptions = [];
+  let key = null;
+  let isPressEnterTwice = false;
+
+  chrome.storage.sync.get(function(storage) {
+    if (storage.exceptions === undefined) {
+      save();
+    }
+    else {
+      exceptions = storage.exceptions;
+    }
+  });
+
   window.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter' && document.activeElement.nodeName === 'INPUT' && document.activeElement.getAttribute('type') === 'text') {
+    if (document.activeElement.nodeName === 'INPUT') {
+      if ((key === 'Enter') && (event.key === 'Enter')) {
+        isPressEnterTwice = true;
+      }
+      else {
+        isPressEnterTwice = false;
+      }
+      key = event.key;
+    }
+
+    if ((isPressEnterTwice === false) && (event.key === 'Enter') && (exceptions.indexOf(window.location.href) < 0)) {
       event.preventDefault();
       return false;
     }
   }, true);
+
+  window.addEventListener('submit', function(event) {
+    if (exceptions.indexOf(window.location.href) < 0) {
+      if (isPressEnterTwice) {
+        confirm = window.prompt('Are you sure?');
+        switch (confirm) {
+          case 'sure':
+          // do nothing. just confirming the submission.
+          break;
+          case 'disable':
+          exceptions.push(window.location.href);
+          save();
+          break;
+          default:
+          event.preventDefault();
+          return false;
+          break;
+        }
+      }
+      else {
+        event.preventDefault();
+        return false;
+      }
+    }
+  }, true);
+
+  function save() {
+    chrome.storage.sync.set({exceptions: exceptions});
+  }
 
 })();
